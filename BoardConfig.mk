d97d99bffdf4579d7e683feb73a449082c3affa4
Peter Cai <peter@typeblog.net>
2020-03-02 17:51:07 +0800

    avicii: sepolicy: Pull in QCOM generic platform sepolicy
    * This makes part of our work easier.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,6 +138,17 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
 
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/private \
+    device/qcom/sepolicy/qva/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/public \
+    device/qcom/sepolicy/qva/public
+
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
