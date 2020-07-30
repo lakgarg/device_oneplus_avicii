5146e3fe60c8749ccc0223860c1f9e162c2caf16
Lakshay Garg <lakshaygarg.in@gmail.com>
2020-07-30 23:32:25 +0530

    nord: set default screen density as 420

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,9 @@ USE_XML_AUDIO_POLICY_CONF := 1
 TARGET_BOOTLOADER_BOARD_NAME := lito
 TARGET_NO_BOOTLOADER := true
 
+# Display
+TARGET_SCREEN_DENSITY := 420
+
 # Kernel
 BOARD_KERNEL_BASE := 0x0000
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 cgroup.memory=nokmem,nosocket console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 loop.max_part=7 lpm_levels.sleep_disabled=1 swiotlb=2048 msm_rtb.filter=0x237 service_locator.enable=1
